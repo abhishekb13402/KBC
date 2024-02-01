bdece8524e947493b67b10183067677b27103952
demo <jadavmayur800@gmail.comgit config --listgit config --global user.email jadavmayur800@gmail.com>
2024-02-01 15:57:42 +0530

    form3 and form3 design

--- KBC/Form3.Designer.cs
+++ KBC/Form3.Designer.cs
@@ -28,33 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btndisplay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // Insert
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(252, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "form 3";
+            this.Insert.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insert.Location = new System.Drawing.Point(892, 178);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(95, 40);
+            this.Insert.TabIndex = 1;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = false;
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update.Location = new System.Drawing.Point(892, 255);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(95, 40);
+            this.Update.TabIndex = 2;
+            this.Update.Text = "update";
+            this.Update.UseVisualStyleBackColor = false;
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.Red;
+            this.Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(892, 333);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(95, 37);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(830, 443);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btndisplay
+            // 
+            this.btndisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btndisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndisplay.Location = new System.Drawing.Point(892, 125);
+            this.btndisplay.Name = "btndisplay";
+            this.btndisplay.Size = new System.Drawing.Size(95, 32);
+            this.btndisplay.TabIndex = 5;
+            this.btndisplay.Text = "display";
+            this.btndisplay.UseVisualStyleBackColor = false;
+            this.btndisplay.Click += new System.EventHandler(this.Display);
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btndisplay);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Insert);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form3";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btndisplay;
     }
 }
