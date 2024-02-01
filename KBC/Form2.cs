fc491c59d6d2fbf946a4e91ca717283fb9769f2e
abhishekb13402 <abhishekbhatt13402@gmail.com>
2024-02-01 12:33:38 +0530

    add code for login guest and admin user

--- KBC/Form2.cs
+++ KBC/Form2.cs
@@ -25,24 +25,59 @@ namespace KBC
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            txt2= textBox1.Text;
-            //hide form2
-            this.Hide();
-            //create time interval for the form1
-            Form1 form1 = new Form1();
-            form1.ShowDialog();//show form1
-            //close form1 interval
-            form1 = null;
-            //Now close form2 and come on form1
-            this.Close();
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+           
 
-        }
+        //}
 
         private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guestuserbtnsubmit_Click(object sender, EventArgs e)
+        {
+            if (guestusername.Text == "user" && guestuserpassword.Text == "user")
+            {
+                txt2 = questionattendno.Text;
+                //hide form2
+                this.Hide();
+                //create time interval for the form1
+                Form1 form1 = new Form1();
+                form1.ShowDialog();//show form1
+                                   //close form1 interval
+                //form1 = null;
+                //Now close form2 and come on form1
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Enter valid id password");
+            }
+        }
+
+        private void adminbtnsubmit_Click(object sender, EventArgs e)
+        {
+            if (adminusername.Text == "admin" && adminuserpassword.Text == "admin")
+            {
+                this.Hide();
+                Form3 form3 = new Form3();
+                form3.ShowDialog();//show form1
+                                   //close form1 interval
+                //form2 = null;
+                //Now close form2 and come on form1
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Enter valid id password");
+            }
         }
     }
 }
