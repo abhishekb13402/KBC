07ab7a2bc0be187aa364aa021660b9ce266ac648
demo <jadavmayur800@gmail.comgit config --listgit config --global user.email jadavmayur800@gmail.com>
2024-02-01 18:10:50 +0530

    Form3 updated UI

--- KBC/Form3.Designer.cs
+++ KBC/Form3.Designer.cs
@@ -31,89 +31,242 @@
             this.Insert = new System.Windows.Forms.Button();
             this.Update = new System.Windows.Forms.Button();
             this.Delete = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgview = new System.Windows.Forms.DataGridView();
             this.btndisplay = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbq_id = new System.Windows.Forms.TextBox();
+            this.tbq_name = new System.Windows.Forms.TextBox();
+            this.tbq_optd = new System.Windows.Forms.TextBox();
+            this.tbq_optc = new System.Windows.Forms.TextBox();
+            this.tbq_optb = new System.Windows.Forms.TextBox();
+            this.tbq_opta = new System.Windows.Forms.TextBox();
+            this.tbq_Correctopt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgview)).BeginInit();
             this.SuspendLayout();
             // 
             // Insert
             // 
             this.Insert.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Insert.Location = new System.Drawing.Point(669, 145);
-            this.Insert.Margin = new System.Windows.Forms.Padding(2);
+            this.Insert.Location = new System.Drawing.Point(1372, 183);
+            this.Insert.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Insert.Name = "Insert";
-            this.Insert.Size = new System.Drawing.Size(71, 32);
+            this.Insert.Size = new System.Drawing.Size(95, 39);
             this.Insert.TabIndex = 1;
             this.Insert.Text = "Insert";
             this.Insert.UseVisualStyleBackColor = false;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
             // 
             // Update
             // 
             this.Update.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Update.Location = new System.Drawing.Point(669, 207);
-            this.Update.Margin = new System.Windows.Forms.Padding(2);
+            this.Update.Location = new System.Drawing.Point(1372, 249);
+            this.Update.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Update.Name = "Update";
-            this.Update.Size = new System.Drawing.Size(71, 32);
+            this.Update.Size = new System.Drawing.Size(95, 39);
             this.Update.TabIndex = 2;
             this.Update.Text = "update";
             this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
             // 
             // Delete
             // 
             this.Delete.BackColor = System.Drawing.Color.Red;
             this.Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Delete.Location = new System.Drawing.Point(669, 271);
-            this.Delete.Margin = new System.Windows.Forms.Padding(2);
+            this.Delete.Location = new System.Drawing.Point(1372, 309);
+            this.Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Delete.Name = "Delete";
-            this.Delete.Size = new System.Drawing.Size(71, 30);
+            this.Delete.Size = new System.Drawing.Size(95, 37);
             this.Delete.TabIndex = 3;
             this.Delete.Text = "Delete";
             this.Delete.UseVisualStyleBackColor = false;
             this.Delete.Click += new System.EventHandler(this.button3_Click);
             // 
-            // dataGridView1
+            // dgview
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 33);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(622, 360);
-            this.dataGridView1.TabIndex = 4;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgview.Location = new System.Drawing.Point(12, 415);
+            this.dgview.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgview.Name = "dgview";
+            this.dgview.RowHeadersWidth = 51;
+            this.dgview.RowTemplate.Height = 24;
+            this.dgview.Size = new System.Drawing.Size(1491, 348);
+            this.dgview.TabIndex = 4;
+            this.dgview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgview.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
             // btndisplay
             // 
             this.btndisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btndisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btndisplay.Location = new System.Drawing.Point(669, 102);
-            this.btndisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.btndisplay.Location = new System.Drawing.Point(1372, 131);
+            this.btndisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btndisplay.Name = "btndisplay";
-            this.btndisplay.Size = new System.Drawing.Size(82, 28);
+            this.btndisplay.Size = new System.Drawing.Size(109, 34);
             this.btndisplay.TabIndex = 5;
             this.btndisplay.Text = "display";
             this.btndisplay.UseVisualStyleBackColor = false;
             this.btndisplay.Click += new System.EventHandler(this.Display);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "q_id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(106, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "q_name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(669, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "q_optA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(669, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "q_optB";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(669, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "q_optC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(669, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "q_optd";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(84, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "q_Correctopt";
+            // 
+            // tbq_id
+            // 
+            this.tbq_id.Location = new System.Drawing.Point(259, 140);
+            this.tbq_id.Name = "tbq_id";
+            this.tbq_id.Size = new System.Drawing.Size(289, 22);
+            this.tbq_id.TabIndex = 13;
+            // 
+            // tbq_name
+            // 
+            this.tbq_name.Location = new System.Drawing.Point(259, 187);
+            this.tbq_name.Multiline = true;
+            this.tbq_name.Name = "tbq_name";
+            this.tbq_name.Size = new System.Drawing.Size(289, 101);
+            this.tbq_name.TabIndex = 14;
+            // 
+            // tbq_optd
+            // 
+            this.tbq_optd.Location = new System.Drawing.Point(833, 309);
+            this.tbq_optd.Name = "tbq_optd";
+            this.tbq_optd.Size = new System.Drawing.Size(302, 22);
+            this.tbq_optd.TabIndex = 15;
+            // 
+            // tbq_optc
+            // 
+            this.tbq_optc.Location = new System.Drawing.Point(833, 249);
+            this.tbq_optc.Name = "tbq_optc";
+            this.tbq_optc.Size = new System.Drawing.Size(302, 22);
+            this.tbq_optc.TabIndex = 16;
+            // 
+            // tbq_optb
+            // 
+            this.tbq_optb.Location = new System.Drawing.Point(833, 186);
+            this.tbq_optb.Name = "tbq_optb";
+            this.tbq_optb.Size = new System.Drawing.Size(302, 22);
+            this.tbq_optb.TabIndex = 17;
+            // 
+            // tbq_opta
+            // 
+            this.tbq_opta.Location = new System.Drawing.Point(833, 131);
+            this.tbq_opta.Name = "tbq_opta";
+            this.tbq_opta.Size = new System.Drawing.Size(302, 22);
+            this.tbq_opta.TabIndex = 18;
+            // 
+            // tbq_Correctopt
+            // 
+            this.tbq_Correctopt.Location = new System.Drawing.Point(259, 309);
+            this.tbq_Correctopt.Name = "tbq_Correctopt";
+            this.tbq_Correctopt.Size = new System.Drawing.Size(289, 22);
+            this.tbq_Correctopt.TabIndex = 19;
+            // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1534, 774);
+            this.Controls.Add(this.tbq_Correctopt);
+            this.Controls.Add(this.tbq_opta);
+            this.Controls.Add(this.tbq_optb);
+            this.Controls.Add(this.tbq_optc);
+            this.Controls.Add(this.tbq_optd);
+            this.Controls.Add(this.tbq_name);
+            this.Controls.Add(this.tbq_id);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btndisplay);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgview);
             this.Controls.Add(this.Delete);
             this.Controls.Add(this.Update);
             this.Controls.Add(this.Insert);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "q_id";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form3_FormClosed);
             this.Load += new System.EventHandler(this.Form3_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgview)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -121,7 +274,21 @@
         private System.Windows.Forms.Button Insert;
         private System.Windows.Forms.Button Update;
         private System.Windows.Forms.Button Delete;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgview;
         private System.Windows.Forms.Button btndisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbq_id;
+        private System.Windows.Forms.TextBox tbq_name;
+        private System.Windows.Forms.TextBox tbq_optd;
+        private System.Windows.Forms.TextBox tbq_optc;
+        private System.Windows.Forms.TextBox tbq_optb;
+        private System.Windows.Forms.TextBox tbq_opta;
+        private System.Windows.Forms.TextBox tbq_Correctopt;
     }
 }
